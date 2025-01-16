de856f203b99eb72458736517aa60c81356b1360
Nikola Irinchev <irinchev@me.com>
2025-01-17 00:32:31 +0100

    add test workflow

---
+++ index.js
@@ -0,0 +1,34 @@
+import { MongoCluster } from 'mongodb-runner';
+import * as path from 'path';
+import * as os from 'os';
+
+const serverOidcConfig = {
+    issuer: 'https://test.com',
+    clientId: 'testServer',
+    requestScopes: ['mongodbGroups'],
+    authorizationClaim: 'groups',
+    audience: 'resource-server-audience-value',
+    authNamePrefix: 'dev',
+  };
+
+await MongoCluster.start({
+    version: '8.0.x',
+      downloadOptions: { enterprise: true },
+      args: [],
+      tmpDir: path.join(os.tmpdir(), `vscode-tests-aaa-data`),
+      topology: 'standalone',
+      logDir: process.env.MONGODB_RUNNER_LOGDIR,
+      args: [
+        '--setParameter',
+        'authenticationMechanisms=SCRAM-SHA-256,MONGODB-OIDC',
+        // enableTestCommands allows using http:// issuers such as http://localhost
+        '--setParameter',
+        'enableTestCommands=true',
+        '--setParameter',
+        `oidcIdentityProviders=${JSON.stringify([serverOidcConfig])}`,
+      ],
+
+});
+
+console.log("success!");
+process.exit(0);
